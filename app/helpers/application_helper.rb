fd085b56eef5ef8c686781d2403208187f1224bd
DYM <grunge.rock.seven@gmail.com>
2017-01-11 13:31:14 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #ページごとの完全なタイトルを消します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample app"
+    if page_title.empty?
+      base_title
+    else
+      page_title + "I" + base_title
+    end
+  end
 end
